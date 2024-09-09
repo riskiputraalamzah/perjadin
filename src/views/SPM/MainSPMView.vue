de4c7b4f3680b5e994b2935aa241f2c5c94fec61
riskiputraalamzah <aryainggil3@gmail.com>
2024-09-10 00:33:40 +0700

    revisi file main spm

--- src/views/SPM/MainSPMView.vue
+++ src/views/SPM/MainSPMView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import ModalItem from '@/components/ModalItem.vue'
+
 import { useIDBStore } from '@/stores/IDB'
 import Swal from 'sweetalert2'
 import { Toast } from '@/components/ToastAlert'
@@ -8,12 +8,6 @@ import { Toast } from '@/components/ToastAlert'
 const idbStore = useIDBStore()
 
 const objectStore = 'spm'
-
-const dataSPM = ref({
-  noSPM: '',
-  tgl: '',
-  nilaiSPM: ''
-})
 
 const listSPM = ref([])
 
@@ -25,65 +19,6 @@ onMounted(async () => {
   listSPM.value = await idbStore.fetchData(objectStore)
   loading.value = !loading.value
 })
-
-const validateForm = () => {
-  if (!dataSPM.value.noSPM) {
-    Toast.fire({ icon: 'error', title: 'Nomor SPM harus diisi' })
-    return false
-  }
-  if (!dataSPM.value.tgl) {
-    Toast.fire({ icon: 'error', title: 'Tanggal SPM harus diisi' })
-    return false
-  }
-  if (!dataSPM.value.nilaiSPM) {
-    Toast.fire({ icon: 'error', title: 'Nilai SPM harus diisi' })
-    return false
-  }
-
-  return true
-}
-
-// Fungsi untuk menyimpan data pegawai
-const handleConfirm = async ({ actionType }) => {
-  if (!validateForm()) return
-  try {
-    const jsonData = JSON.parse(JSON.stringify(dataSPM.value))
-    if (actionType === 'add') {
-      await idbStore.addItem(objectStore, jsonData)
-      Toast.fire({ icon: 'success', title: 'Data Surat Tugas berhasil ditambahkan' })
-    } else if (actionType === 'edit') {
-      await idbStore.updateData(objectStore, 'id', jsonData.id, jsonData)
-      Toast.fire({ icon: 'success', title: 'Data Surat Tugas berhasil diubah' })
-    }
-    listSPM.value = await idbStore.fetchData(objectStore)
-    resetForm()
-
-    // Menutup modal menggunakan Bootstrap Modal API
-    const modalElement = document.getElementById('staticBackdrop')
-    const modalInstance = window.bootstrap.Modal.getInstance(modalElement)
-    if (modalInstance) {
-      modalInstance.hide() // Menutup modal
-    }
-
-    // Hapus backdrop secara manual jika masih ada
-    const backdrop = document.querySelector('.modal-backdrop')
-    if (backdrop) {
-      backdrop.remove()
-    }
-  } catch (error) {
-    console.error('Error:', error)
-    Toast.fire({ icon: 'error', title: 'Terjadi kesalahan' })
-  }
-}
-
-// Reset form setelah data disimpan
-const resetForm = () => {
-  dataSPM.value = {
-    noSPM: '',
-    tgl: '',
-    nilaiSPM: ''
-  }
-}
 
 const handleDelete = async (id) => {
   // Konfirmasi dengan SweetAlert
@@ -115,37 +50,6 @@ const handleDelete = async (id) => {
     }
   }
 }
-
-const openAddModal = () => {
-  resetForm()
-  modalProps.value = {
-    title: 'Tambah Data',
-    confirmButtonText: 'Simpan',
-    actionType: 'add',
-    data: dataSPM.value
-  }
-  // document.getElementById('staticBackdrop').click()
-}
-
-// Fungsi untuk menyiapkan modal edit
-const openEditModal = (data) => {
-  dataSPM.value = { ...data }
-  modalProps.value = {
-    title: 'Edit Data',
-    confirmButtonText: 'Update',
-    actionType: 'edit',
-    data: dataSPM.value
-  }
-
-  // document.getElementById('staticBackdrop').click()
-}
-
-const modalProps = ref({
-  title: 'Tambah Data',
-  confirmButtonText: 'Simpan',
-  actionType: 'add',
-  data: {}
-})
 </script>
 
 <template>
@@ -190,37 +94,5 @@ const modalProps = ref({
         </tbody>
       </table>
     </div>
-
-    <!-- Modal Tambah/Edit data -->
-    <ModalItem
-      :title="modalProps.title"
-      :confirmButtonText="modalProps.confirmButtonText"
-      :actionType="modalProps.actionType"
-      :data="modalProps.data"
-      @confirm="handleConfirm"
-    >
-      <template #body>
-        <form @submit.prevent="handleConfirm">
-          <div class="mb-3">
-            <label for="noST" class="form-label">Nomor SPM</label>
-            <input v-model="dataSPM.noSPM" type="text" class="form-control" id="noST" />
-          </div>
-          <div class="mb-3">
-            <label for="tgl" class="form-label">Tanggal SPM</label>
-            <input v-model="dataSPM.tgl" type="date" class="form-control" id="tgl" />
-          </div>
-          <div class="mb-3">
-            <label for="nilaiSPM" class="form-label">Nilai SPM</label>
-            <input
-              v-model="dataSPM.noSPM"
-              readonly
-              type="text"
-              class="form-control"
-              id="nilaiSPM"
-            />
-          </div>
-        </form>
-      </template>
-    </ModalItem>
   </div>
 </template>
